681c1cfcb0381a8f9e73c742e3536f8654e5deb7
RobertSellers <robert.sellers@azimuth1.com>
2018-01-19 12:33:16 -0500

    111

--- R/uploaddata.R
+++ R/uploaddata.R
@@ -2,6 +2,6 @@ uploaddata <- function(mipfile, ...){
   if(substring(tolower(mipfile), nchar(mipfile)-3) != ".mhp"){
     stop('Uploaded data needs to be .mhp file. ');
   }
-    mydata <- read.table(mipfile, sep="\t", header=TRUE)
+  mydata <- read.table(mipfile, sep="\t", header=TRUE)
   return(mydata)  
 }

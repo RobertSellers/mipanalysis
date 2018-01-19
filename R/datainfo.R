3bf8c718003ad7337b6d93287aadf2675b99a6ca
RobertSellers <robert.sellers@azimuth1.com>
2018-01-19 14:48:41 -0500

    housekeeping

--- R/datainfo.R
+++
@@ -1,7 +0,0 @@
-datainfo <- function(mydata){
-  list(
-    all = as.list(colnames(mydata)),
-    strings = as.list(names(which(unlist(lapply(mydata, is.character))))),
-    other = as.list(names(which(unlist(lapply(mydata, function(x){!is.character(x)})))))
-  )
-}
--- R/mapCountryData.R
+++
@@ -1,4 +0,0 @@
-map <- function(mapToPlot, nameColumnToPlot, ...){
-  par(mai=c(0,0,0.2,0), xaxs="i", yaxs="i");
-  mapCountryData(mapToPlot, nameColumnToPlot, ...)
-}

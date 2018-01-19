d5389443869a06016a1c6a988b264d2fe234b5bf
RobertSellers <robert.sellers@azimuth1.com>
2018-01-19 17:06:02 -0500

    colnames fixed

--- R/uploaddata.R
+++ R/uploaddata.R
@@ -3,7 +3,41 @@ uploaddata <- function(mipfile, ...){
     stop('Uploaded data needs to be .zip file. ');
   }
   mhp_filename <- paste0(substr(basename(mipfile),0,nchar(basename(mipfile))-8),".mhp")
-  mip_file_data <- clean_names(read.table(unz(mipfile, mhp_filename), header=T, quote="\"", sep="\t"))
-  mip_file_data$test = 0
+  # mip_file_data <- clean_names(read.table(unz(mipfile, mhp_filename), header=T, quote="\"", sep="\t"))
+  col_names <- c("Depth (ft)",	
+                  "EC (mS/m)",	
+                  "ROP (ft/min)",	
+                  "Temp. Min (deg C)",	
+                  "Temp. Max (deg C)",	
+                  "MIP Pressure (psi)",	
+                  "MIP Flow (mL/min)",	
+                  "Depth (m)",	
+                  "ROP (m/min)",
+                  "MIP Pressure (kPa)",
+                  "Detector 1 Min (uV)",
+                  "Detector 1 Max (uV)",
+                  "Detector 2 Min (uV)",
+                  "Detector 2 Max (uV)",
+                  "Detector 3 Min (uV)",
+                  "Detector 3 Max (uV)",
+                  "Detector 4 Min (uV)",
+                  "Detector 4 Max (uV)",
+                  "HPT Press. Min (psi)",
+                  "HPT Press. Avg (psi)",
+                  "HPT Press. Max (psi)",
+                  "HPT Flow Min (mL/min)",
+                  "HPT Flow Avg (mL/min)",
+                  "HPT Flow Max (mL/min)",
+                  "HPT Line Press. Min (psi)",
+                  "HPT Line Press. Avg (psi)",
+                  "HPT Line Press. Max (psi)",
+                  "HPT Press. Min (kPa)",
+                  "HPT Press. Avg (kPa)",
+                  "HPT Press. Max (kPa)",
+                  "HPT Line Press. Min (kPa)",
+                  "HPT Line Press. Avg (kPa)",
+                  "HPT Line Press. Max (kPa)",
+                  "HPT Screen Depth (ft)")
+  colnames(mip_file_data) <- col_names
   return(mip_file_data)
 }

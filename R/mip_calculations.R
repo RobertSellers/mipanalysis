d7c899eac0825ed4827accc6daa17ff778adee34
RobertSellers <robert.sellers@azimuth1.com>
2018-01-19 18:53:20 -0500

    revetert

--- R/mip_calculations.R
+++ R/mip_calculations.R
@@ -1,5 +1,40 @@
 mip_calculations <- function(mipfile, ...){
     mip_file_data <- read.table(mipfile, header=T, quote="\"", sep="\t")
+    col_names <- c("Depth (ft)",	
+                    "EC (mS/m)",	
+                    "ROP (ft/min)",	
+                    "Temp. Min (deg C)",	
+                    "Temp. Max (deg C)",	
+                    "MIP Pressure (psi)",	
+                    "MIP Flow (mL/min)",	
+                    "Depth (m)",	
+                    "ROP (m/min)",
+                    "MIP Pressure (kPa)",
+                    "Detector 1 Min (uV)",
+                    "Detector 1 Max (uV)",
+                    "Detector 2 Min (uV)",
+                    "Detector 2 Max (uV)",
+                    "Detector 3 Min (uV)",
+                    "Detector 3 Max (uV)",
+                    "Detector 4 Min (uV)",
+                    "Detector 4 Max (uV)",
+                    "HPT Press. Min (psi)",
+                    "HPT Press. Avg (psi)",
+                    "HPT Press. Max (psi)",
+                    "HPT Flow Min (mL/min)",
+                    "HPT Flow Avg (mL/min)",
+                    "HPT Flow Max (mL/min)",
+                    "HPT Line Press. Min (psi)",
+                    "HPT Line Press. Avg (psi)",
+                    "HPT Line Press. Max (psi)",
+                    "HPT Press. Min (kPa)",
+                    "HPT Press. Avg (kPa)",
+                    "HPT Press. Max (kPa)",
+                    "HPT Line Press. Min (kPa)",
+                    "HPT Line Press. Avg (kPa)",
+                    "HPT Line Press. Max (kPa)",
+                    "HPT Screen Depth (ft)")
+    colnames(mip_file_data) <- col_names
     mip_file_data$test = 0
     return(mip_file_data)
 }

a50b6b5593ae9f1aee9277c7c247f52280db98e1
RobertSellers <robert.sellers@azimuth1.com>
2018-01-19 11:47:27 -0500

    data loading test

--- R/uploaddata.R
+++ R/uploaddata.R
@@ -1,10 +1,10 @@
-uploaddata <- function(csvfile, ...){
-  if(substring(tolower(csvfile), nchar(csvfile)-3) != ".csv"){
+uploaddata <- function(mipfile, ...){
+  if(substring(tolower(mipfile), nchar(mipfile)-3) != ".csv"){
     stop('Uploaded data needs to be .csv file. When using MS Excel, try "Save As" and select type "CSV (comma delimited)"');
   }
   
   #read csv data
-  mydata <- read.csv(csvfile, row.names = NULL, stringsAsFactors=FALSE, ...);
+  mydata <- read.csv(mipfile, row.names = NULL, stringsAsFactors=FALSE, ...);
   
   #convert columns with 7 or less levels to factors
   for(i in seq_along(mydata)){

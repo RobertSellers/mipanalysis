e82bcb45efd5d9268cd413d8cb93223b4f07b7ba
RobertSellers <robert.sellers@azimuth1.com>
2018-01-19 12:28:30 -0500

    1

--- R/uploaddata.R
+++ R/uploaddata.R
@@ -2,18 +2,6 @@ uploaddata <- function(mipfile, ...){
   if(substring(tolower(mipfile), nchar(mipfile)-3) != ".mhp"){
     stop('Uploaded data needs to be .mhp file. ');
   }
-  
-  #read csv data
-  # mydata <- read.csv(mipfile, row.names = NULL, stringsAsFactors=FALSE, ...);
-  read.table(mipfile, sep="\t", header=TRUE)
-
-  #convert columns with 7 or less levels to factors
-  # for(i in seq_along(mydata)){
-  #   if(length(unique(mydata[[i]])) < 8){
-  #     mydata[[i]] <- as.factor(mydata[[i]]);
-  #   }
-  # }
-  
-  #return dataset
+    mydata <- read.table(mipfile, sep="\t", header=TRUE)
   return(mydata)  
 }

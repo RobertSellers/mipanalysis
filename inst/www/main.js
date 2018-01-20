527251f97f9c1bcf349b2d93434fd071dee173dc
RobertSellers <robert.sellers@azimuth1.com>
2018-01-19 23:11:56 -0500

    fix to remove quotes

--- inst/www/main.js
+++ inst/www/main.js
@@ -1,51 +1,51 @@
 $(document).ready(function() {
   
+  $("#convert_button").attr("disabled", "disabled");
+  $("#water_level").attr("disabled", "disabled");
+
+  //automatically upload CSV file on change.
+  $("#mipfile").on("change", function(){
+
+    //verify that a file is selected
+    if($("#mipfile")[0].files[0]){
+
+      $("#successdiv").empty();
+      $("#errordiv").empty()
+
+      var req = ocpu.call("uploaddata", {
+        mipfile : $("#mipfile")[0].files[0]
+      }, function(session){
+        $("#convert_button").attr("href", session.getLoc())
+        $("#water_level").removeAttr("disabled");
+        $("#convert_button").removeAttr("disabled");
+        $("#step_2").css("opacity", 1);
+        $("#step_3").css("opacity", 1);
+      }).fail(function(jqXHR){
+        $("#step_2").css("opacity", 0.2);
+        $("#step_3").css("opacity", 0.2);
+        errormsg(jqXHR.responseText);
+      })
+    }
+  });
+  
+  function successmsg(text){
+    $("#successdiv").empty().append('<div class="alert alert-success alert-dismissable"><a href="#" class="close" data-dismiss="alert">&times;</a>' + text + '</div>');
+  }
+  
+  function errormsg(text){
     $("#convert_button").attr("disabled", "disabled");
     $("#water_level").attr("disabled", "disabled");
-    //automatically upload CSV file on change.
-    $("#mipfile").on("change", function(){
-  
-      //verify that a file is selected
-      if($("#mipfile")[0].files[0]){
-  
-        $("#successdiv").empty();
-        $("#errordiv").empty()
-  
-        var req = ocpu.call("uploaddata", {
-          mipfile : $("#mipfile")[0].files[0]   
-        }, function(session){
-          $("#convert_button").attr("href", session.getLoc())
-          $("#water_level").removeAttr("disabled");
-          $("#convert_button").removeAttr("disabled");
-          $("#step_2").css("opacity", 1);
-          $("#step_3").css("opacity", 1);
-        }).fail(function(jqXHR){
-          $("#step_2").css("opacity", 0.2);
-          $("#step_3").css("opacity", 0.2);
-          errormsg(jqXHR.responseText);
-        })
-      }
-    });
-    
-    function successmsg(text){
-      $("#successdiv").empty().append('<div class="alert alert-success alert-dismissable"><a href="#" class="close" data-dismiss="alert">&times;</a>' + text + '</div>');
-    }
-    
-    function errormsg(text){
-      $("#convert_button").attr("disabled", "disabled");
-      $("#water_level").attr("disabled", "disabled");
-      $("#errordiv").empty().append('<div class="alert alert-danger alert-dismissable"><a href="#" class="close" data-dismiss="alert">&times;</a>' + text + '</div>');
-    }  
+    $("#errordiv").empty().append('<div class="alert alert-danger alert-dismissable"><a href="#" class="close" data-dismiss="alert">&times;</a>' + text + '</div>');
+  }  
       
   function create_zip(data_file){
-  
     $.get(file_names, function(f){
-  
+      debugger
       var zip = new JSZip();
-  
       zipname = f.split('\n')[0]
       filename = zipname.substr(0, f.split('\n')[0].length -8);
         $.get(data_file, function(d){
+
           zip.file(filename+'.mhp', d)
           zip.generateAsync({type:"blob"})
           .then(function(content) {
@@ -55,7 +55,7 @@ $(document).ready(function() {
     });   
   }
   
-    $("#convert_button").on("click", function(){
+  $("#convert_button").on("click", function(){
   
       file_names = $(this).attr('href')+'files'
       
@@ -68,15 +68,15 @@ $(document).ready(function() {
       })
   
   
-    });
-      
-    $(document).ajaxStart(function() {
-      $(".progress").show();
-    }); 
-    
-    $(document).ajaxStop(function() {
-      $(".progress").hide();
-    });
-    
-    
   });
+      
+  $(document).ajaxStart(function() {
+    $(".progress").show();
+  }); 
+  
+  $(document).ajaxStop(function() {
+    $(".progress").hide();
+  });
+  
+    
+});
